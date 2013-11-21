fdbb89b3cd67e9c38cfad53d7ed91af78022e8d8
scristia <sc759@cornell.edu>
2013-11-20 19:41:15 -0500

    make lattice plot

--- exploratory/checkbatch.R
+++ exploratory/checkbatch.R
@@ -4,10 +4,10 @@ library(MixtureModel)
 library(plyr)
 
 ## Read in genomic ranges data and avg R ratios
-avgRs <- readRDS("~/MixtureModels/data/avgRs.rds")
-granges <- readRDS("~/MixtureModels/data/granges.rds")
-reduced <- readRDS("~/MixtureModels/data/reduced.rds")
-grl <- readRDS("~/MixtureModels/data/vi_grl.rds")
+avgRs <- readRDS("~scristia/MixtureModels/data/avgRs.rds")
+granges <- readRDS("~scristia/MixtureModels/data/granges.rds")
+reduced <- readRDS("~scristia/MixtureModels/data/reduced.rds")
+grl <- readRDS("~scristia/MixtureModels/data/vi_grl.rds")
 subject.cnvs <- unlist(grl)
 
 olap <- findOverlaps(reduced, subject.cnvs)
@@ -17,12 +17,12 @@ indices <- split(subjectHits(olap), queryHits(olap))
 inds <- queryHits(findOverlaps(granges, reduced))
 
 ## READ IN PLATES
-plates <- readRDS("~/MixtureModels/data/plate.rds")
-lrr.list0 <- readRDS("~/MixtureModels/data/LRRList_red.rds")
-nainds <- readRDS("~/nainds.rds")
-samples <- readRDS("~/MixtureModels/data/samples.RDS")
+plates <- readRDS("~scristia/MixtureModels/data/plate.rds")
+lrr.list0 <- readRDS("~scristia/MixtureModels/data/LRRList_adj.rds")
+nainds <- readRDS("~scristia/nainds.rds")
+samples <- readRDS("~scristia/MixtureModels/data/samples.RDS")
 
-source("~/MixtureModels/findnas.R")
+source("~scristia/MixtureModels/findnas.R")
 ## find and remove markers with NAs
 lrr.list0 <- sapply(lrr.list0, function(x) x[, -nainds, drop=FALSE])
 nonas <- function(mat) {
@@ -38,7 +38,7 @@ rm(nonas)
 ## Need to remove plates with less than 50 subjects
 plates0 <- plates[-nainds]
 plates.rle <- rle(plates0)
-w <- plates.rle$values[plates.rle$lengths < 50]
+w <- plates.rle$values[plates.rle$lengths < 67]
 
 ## names of plates with under 20 samples
 wind <- which(!is.na(match(plates0, w)))
@@ -51,10 +51,10 @@ avgRs0 <- t(sapply(lrr.list2, colMeans))
 
 
 ## calculate f-statistics for each region using ANOVA
-pval <- c()
-fstat <- c()
-for(i in seq_along(avgRs0)) {
-    a <- aov(avgRs0[,i] ~ plates0[-wind])
+pval <- rep(NA, length(avgRs0[,1]))
+fstat <-rep(NA, length(avgRs0[,1]))
+for(i in seq_along(avgRs0[,1])) {
+    a <- aov(avgRs0[i,]/100 ~ plates0[-wind])
     pval[i] <- summary(a)[[1]][["Pr(>F)"]][1]
     fstat[i] <- summary(a)[[1]][["F value"]][1]
 }
@@ -65,4 +65,19 @@ meds <- sapply(r.list, median, na.rm=TRUE)
 r.list.centered <- mapply(function(r, med) r-med, r=r.list, med=meds)
 r.centered <- unlist(r.list.centered)
 
+library(lattice)
+## exploratory by batch histograms for region 1
+v <- rle(plates0[-wind])$lengths
+vv <- sum(v[1:20])
+vv2 <- sum(v[21:40])+vv
+d <- data.frame("avgRs"=avgRs0[302,1:vv]/100, "plates"=plates0[-wind][1:vv])
+d2 <- data.frame("avgRs"=avgRs0[302,(vv+1):vv2]/100, "plates"=plates0[-wind][(vv+1):vv2])
+pdf("~/MixtureModels/batch/batch302_%01d.pdf", onefile=FALSE)
+histogram(~avgRs | plates, d, breaks=40, border="gray", col="gray",
+          panel=function(x,...) { panel.histogram(x,...)
+          panel.abline(v=0, col="gray40", lty=2) } , main="Region 302")
+histogram(~avgRs | plates, d2, breaks=40, border="gray", col="gray",
+          panel=function(x,...) { panel.histogram(x,...)
+          panel.abline(v=0, col="gray40", lty=2) } , main="Region 302")
+dev.off()
 
