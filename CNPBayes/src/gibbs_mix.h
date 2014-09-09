5acfd70833d3dbf73166f77448e51019d3a6a297
Rob Scharpf <rscharpf@jhu.edu>
2014-09-09 09:32:47 -0400

    Replaced include <Rcpp.h> statements with <RcppArmadillo.h>

--- CNPBayes/src/gibbs_mix.h
+++ CNPBayes/src/gibbs_mix.h
@@ -1,7 +1,7 @@
 #ifndef _MixtureModel_RCPP_GIBBS_MIX_H
 #define _MixtureModel_RCPP_GIBBS_MIX_H
 #include <RcppArmadillo.h>
-#include <Rcpp.h>
+/* #include <Rcpp.h> */
 RcppExport SEXP gibbs_mix(SEXP r, SEXP means, SEXP precs, SEXP P, SEXP Z,
         SEXP nu0, SEXP mu0, SEXP kappa0, SEXP alpha,
         SEXP tau20, SEXP sigma20, SEXP rbar, SEXP s2,
