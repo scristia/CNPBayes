43f886b3eabff49e6e11bdd544e9609a58b24c48
Jacob Carey <jacobcvt12@gmail.com>
2015-06-10 10:42:08 -0400

    Document MixtureModel class.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -97,6 +97,34 @@ setClass("McmcParams", representation(thin="numeric",
                                       nstarts="numeric",
                                       param_updates="integer"))
 
+#' An object for running MCMC simulations.
+#'
+#' BatchModel and MarginalModel both inherit from this class.
+#' @slot k An integer value specifying the number of latent classes.
+#' @slot hyperparams An object of class `Hyperparameters` used to specify the hyperparameters of the model.
+#' @slot theta the means of each component and batch
+#' @slot sigma2 the variances of each component and batch
+#' @slot nu.0 the shape parameter for sigma2
+#' @slot sigma2.0 the rate parameter for sigma2
+#' @slot pi mixture probabilities which are assumed to be the same for all batches
+#' @slot mu overall mean
+#' @slot tau2 overall variance
+#' @slot data the data for the simulation.
+#' @slot data.mean the empirical means of the components
+#' @slot data.prec the empirical precisions
+#' @slot z latent variables
+#' @slot zfreq table of latent variables
+#' @slot probz n x k matrix of probabilities
+#' @slot logprior log likelihood of prior: log(p(sigma2.0)p(nu.0)p(mu))
+#' @slot loglik log likelihood: $\sum p_k \Phi(\theta_k, \sigma_k)
+#' @slot mcmc.chains an object of class 'McmcChains' to store MCMC samples
+#' @slot batch a vector of the different batch numbers
+#' @slot batchElements a vector labeling from which batch each observation came from
+#' @slot hwe Hardy Weinberg Equilibrium
+#' @slot modes the values of parameters from the iteration which maximizes log likelihood and log prior
+#' @slot theta_order to be removed
+#' @slot m.y to be removed
+#' @slot mcmc.params An object of class 'McmcParams'
 setClass("MixtureModel", representation("VIRTUAL",
                                         k = "integer",
                                         hyperparams="Hyperparameters",
