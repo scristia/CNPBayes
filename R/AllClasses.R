89771a69b6b89eb345dddb61a84951276711a47f
Jacob Carey <jacobcvt12@gmail.com>
2015-06-10 11:02:30 -0400

    Document MarginalModel class.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -180,30 +180,32 @@ setClass("BatchModel", contains="MixtureModel")
 
 #' The 'MarginalModel' class 
 #'
+#' Run marginal MCMC simulation
 #' @slot k An integer value specifying the number of latent classes.
-#' @slot hyperparams An object of class `Hyperparameters` used to specify the hyperparameters of....
-#' @slot theta A
-#' @slot sigma2 A
-#' @slot nu.0 A
-#' @slot sigma2.0 A
-#' @slot pi A
-#' @slot mu A
-#' @slot tau2 A
-#' @slot data A
-#' @slot data.mean A
-#' @slot data.prec A
-#' @slot z A
-#' @slot zfreq A
-#' @slot probz A
-#' @slot logprior A
-#' @slot loglik A
-#' @slot mcmc.chains A
-#' @slot batch A
-#' @slot batchElements A
-#' @slot hwe A
-#' @slot modes A
+#' @slot hyperparams An object of class `Hyperparameters` used to specify the hyperparameters of the model.
+#' @slot theta the means of each component and batch
+#' @slot sigma2 the variances of each component and batch
+#' @slot nu.0 the shape parameter for sigma2
+#' @slot sigma2.0 the rate parameter for sigma2
+#' @slot pi mixture probabilities which are assumed to be the same for all batches
+#' @slot mu overall mean
+#' @slot tau2 overall variance
+#' @slot data the data for the simulation.
+#' @slot data.mean the empirical means of the components
+#' @slot data.prec the empirical precisions
+#' @slot z latent variables
+#' @slot zfreq table of latent variables
+#' @slot probz n x k matrix of probabilities
+#' @slot logprior log likelihood of prior: log(p(sigma2.0)p(nu.0)p(mu))
+#' @slot loglik log likelihood: $\sum p_k \Phi(\theta_k, \sigma_k)
+#' @slot mcmc.chains an object of class 'McmcChains' to store MCMC samples
+#' @slot batch a vector of the different batch numbers
+#' @slot batchElements a vector labeling from which batch each observation came from
+#' @slot hwe Hardy Weinberg Equilibrium
+#' @slot modes the values of parameters from the iteration which maximizes log likelihood and log prior
 #' @slot mcmc.params An object of class 'McmcParams'
 setClass("MarginalModel", contains="MixtureModel")
+
 setClass("UnivariateBatchModel", contains="BatchModel")
 # setClass("UnivariateMarginalModel", contains="MarginalModel")
 
