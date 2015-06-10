817ffcd7b1a505638ec91f73da807fd58e0580dc
Jacob Carey <jacobcvt12@gmail.com>
2015-06-10 11:00:25 -0400

    Document BatchModel class.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -149,7 +149,32 @@ setClass("MixtureModel", representation("VIRTUAL",
                                         modes="list",
                                         mcmc.params="McmcParams"))
 
-
+#' An object for running MCMC simulations.
+#'
+#' Run hierarchical MCMC for batch model.
+#' @slot k An integer value specifying the number of latent classes.
+#' @slot hyperparams An object of class `Hyperparameters` used to specify the hyperparameters of the model.
+#' @slot theta the means of each component and batch
+#' @slot sigma2 the variances of each component and batch
+#' @slot nu.0 the shape parameter for sigma2
+#' @slot sigma2.0 the rate parameter for sigma2
+#' @slot pi mixture probabilities which are assumed to be the same for all batches
+#' @slot mu means from batches, averaged across batches
+#' @slot tau2 variances from batches,  weighted by precisions
+#' @slot data the data for the simulation.
+#' @slot data.mean the empirical means of the components
+#' @slot data.prec the empirical precisions
+#' @slot z latent variables
+#' @slot zfreq table of latent variables
+#' @slot probz n x k matrix of probabilities
+#' @slot logprior log likelihood of prior: log(p(sigma2.0)p(nu.0)p(mu))
+#' @slot loglik log likelihood: $\sum p_k \Phi(\theta_k, \sigma_k)
+#' @slot mcmc.chains an object of class 'McmcChains' to store MCMC samples
+#' @slot batch a vector of the different batch numbers
+#' @slot batchElements a vector labeling from which batch each observation came from
+#' @slot hwe Hardy Weinberg Equilibrium
+#' @slot modes the values of parameters from the iteration which maximizes log likelihood and log prior
+#' @slot mcmc.params An object of class 'McmcParams'
 setClass("BatchModel", contains="MixtureModel")
 
 
