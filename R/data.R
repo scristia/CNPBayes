95166fefab25831a3ef671c3dca7849b1358ecf4
Jacob Carey <jacobcvt12@gmail.com>
2015-06-22 22:21:03 -0400

    Document datasets.

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' This data is a simulated example of Marginal data
+#'
+#' @author Jacob Carey
+"MarginalModelExample"
+
+#' This data is a simulated example of Batch data
+#'
+#' @author Jacob Carey
+"BatchModelExample"
