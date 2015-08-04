3ead3945aa0bac86e7452ff287a9d91113209317
Jacob Carey <jacobcvt12@gmail.com>
2015-08-04 14:19:06 -0400

    Create updates_reduced_marginal.cpp.
    Moved functions from updates_marginal to updates_reduced_marginal.
    Created new header file.

--- src/updates_marginal.h
+++ src/updates_marginal.h
@@ -26,33 +26,4 @@ Rcpp::IntegerMatrix compute_probz(Rcpp::S4 xmod);
 Rcpp::S4 mcmc_marginal_burnin(Rcpp::S4 xmod, Rcpp::S4 mcmcp);
 Rcpp::S4 mcmc_marginal(Rcpp::S4 object, Rcpp::S4 mcmcp);
 
-Rcpp::NumericVector marginal_theta(Rcpp::S4 xmod);
-Rcpp::NumericVector p_theta_zpermuted(Rcpp::S4 xmod);
-
-Rcpp::NumericVector marginal_sigma2(Rcpp::S4 xmod, Rcpp::S4 mcmcp);
-
-Rcpp::S4 simulate_z_reduced1(Rcpp::S4 object);
-Rcpp::S4 simulate_z_reduced2(Rcpp::S4 object);
-Rcpp::S4 permutedz_reduced1(Rcpp::S4 xmod);
-Rcpp::S4 permutedz_reduced2(Rcpp::S4 xmod);
-
-Rcpp::NumericVector p_pmix_reduced(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_sigma(Rcpp::S4 xmod);
-Rcpp::NumericVector p_sigma_reduced(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_pi(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_mu(Rcpp::S4 xmod);
-Rcpp::NumericVector p_mu_reduced(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_tau(Rcpp::S4 xmod);
-Rcpp::NumericVector p_tau_reduced(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_nu0(Rcpp::S4 xmod);
-Rcpp::NumericVector p_nu0_reduced(Rcpp::S4 xmod);
-
-Rcpp::S4 reduced_s20(Rcpp::S4 xmod);
-Rcpp::NumericVector p_s20_reduced(Rcpp::S4 xmod);
-
 #endif
