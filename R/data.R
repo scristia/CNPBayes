fe5fe6d7bd981df915caa35408b4cab1472708c2
Jacob Carey <jacobcvt12@gmail.com>
2015-08-27 22:15:16 -0400

    Update data description.

--- R/data.R
+++ R/data.R
@@ -1,9 +1,13 @@
 #' This data is a simulated example of Marginal data
 #'
+#' @docType data
+#' @usage data(MarginalModelExample)
 #' @author Jacob Carey
 "MarginalModelExample"
 
 #' This data is a simulated example of Batch data
 #'
+#' @docType data
+#' @usage data(BatchModelExample)
 #' @author Jacob Carey
 "BatchModelExample"
