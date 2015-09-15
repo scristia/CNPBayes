00c007342f58e2562f7ad200c48548288ee88210
Jacob Carey <jacobcvt12@gmail.com>
2015-09-15 12:11:27 -0400

    Include Rcpp.h in miscfunctions.h

--- src/miscfunctions.h
+++ src/miscfunctions.h
@@ -1,6 +1,8 @@
 #ifndef _miscfunctions_H
 #define _miscfunctions_H
 
+#include <Rcpp.h>
+
 void rdirichlet(Rcpp::NumericVector, Rcpp::NumericVector);
 double cons_normal(double, double, double, double);
 double trunc_norm(double mean, double sd);
