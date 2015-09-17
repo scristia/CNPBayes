6b8c493d813ade44686a6604c5dbe070ba9fe8d0
Jacob Carey <jacobcvt12@gmail.com>
2015-09-17 12:00:36 -0400

    Move hardTruth function to inside of test.

--- tests/testthat/test_batch.R
+++ tests/testthat/test_batch.R
@@ -111,6 +111,26 @@ test_that("test_batchEasy", {
 test_that("test_hard3", {
     library(oligoClasses)
     library(GenomicRanges)
+    # embed function in test for now
+    hardTruth <- function(prop_comp1=0.005, s=0.3) {
+      set.seed(1234)
+      k <- 3
+      nbatch <- 3
+      means <- matrix(c(-1.9, -2, -1.85,
+                        -0.45, -0.4, -0.35,
+                        -0.1, 0, -0.05), nbatch, k, byrow=FALSE)
+      sds <- matrix(s, nbatch, k)
+      p1 <- prop_comp1
+      p2 <- 20*p1
+      p3 <- 1-p1-p2
+      N <- 8e3
+      truth <- simulateBatchData(N,
+                                 theta=means,
+                                 sds=sds,
+                                 batch=rep(letters[1:3], length.out=N),
+                                 p=c(p1, p2, p3))
+    }
+    # end function
     truth <- hardTruth(0.005, s = 0.1)
     table(z(truth), batch(truth))
     if (FALSE) 
