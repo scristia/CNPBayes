34b13b4793e9bd4b7275944f4e3158aee5ddc5c6
Jacob Carey <jacobcvt12@gmail.com>
2015-09-25 15:59:38 -0400

    Remove posteriorSigma2 generic.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -539,8 +539,6 @@ setGeneric("thin", function(object) standardGeneric("thin"))
 #' @rdname posteriorSimulation-method
 setGeneric("posteriorSimulation", function(object, k) standardGeneric("posteriorSimulation"))
 
-setGeneric("posteriorSigma2", function(object, mcmcp) standardGeneric("posteriorSigma2"))
-
 setGeneric("isMarginalModel", function(object) standardGeneric("isMarginalModel"))
 
 setGeneric("computePrior", function(object) standardGeneric("computePrior"))
