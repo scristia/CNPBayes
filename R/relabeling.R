4a6b3954fe4f5db1753a63b7c84f2064c4fd3f92
Jacob Carey <jcarey15@jhu.edu>
2015-10-26 20:16:07 +0000

    Add relabeling function.
    This is a prototype and will change.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@109932 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/relabeling.R
@@ -0,0 +1,35 @@
+# return fraction that are not relabeled
+# this works for marginal
+# the batch method will work by running this method on each batch
+relabeling <- function(model, merge=TRUE) {
+    # handle case of merge == FALSE
+    if (merge) {
+        merged <- DensityModel(model, merge=TRUE)
+        comp_map <- clusters(merged)
+    }
+
+    comp_orig <- as.integer(names(comp_map))
+    comp_merged <- names(comp_map) != comp_map
+
+    thetas <- theta(chains(model))
+    ordered <- apply(thetas, 1, function(x) paste(order(x), collapse=""))
+
+
+
+    for (i in comp_orig) {
+        if (comp_merged[i]) {
+            ordered <- gsub(as.character(i), as.character(comp_map[i]), ordered)
+        }
+    }
+    `
+    table_ordered <- sort(table(ordered), decreasing=TRUE)
+    proportion <- table_ordered[1] / sum(table_ordered)
+
+    return(proportion)
+}
+
+order_thetas <- function(thetas, comp_map) {
+    if (!missing comp_map) {
+    } else {
+    }
+}
