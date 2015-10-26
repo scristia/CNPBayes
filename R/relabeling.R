dcf3a75c095bfcf40a72f092fa7255d399307644
Jacob Carey <jacobcvt12@gmail.com>
2015-10-26 15:07:46 -0400

    Update relabeling function.
    Now takes chain of thetas (from only one batch)

--- R/relabeling.R
+++ R/relabeling.R
@@ -1,20 +1,11 @@
 # return fraction that are not relabeled
 # this works for marginal
 # the batch method will work by running this method on each batch
-relabeling <- function(model, merge=TRUE) {
-    # handle case of merge == FALSE
-    if (merge) {
-        merged <- DensityModel(model, merge=TRUE)
-        comp_map <- clusters(merged)
-    }
-
+relabeling <- function(thetas, comp_map) {
     comp_orig <- as.integer(names(comp_map))
     comp_merged <- names(comp_map) != comp_map
 
-    thetas <- theta(chains(model))
     ordered <- apply(thetas, 1, function(x) paste(order(x), collapse=""))
-
-
 
     for (i in comp_orig) {
         if (comp_merged[i]) {
