d4cb7f19068a53a83b2d4a5ea22116cf2355200e
Jacob Carey <jacobcvt12@gmail.com>
2015-11-03 11:44:15 -0500

    Add test for overfit batch galaxy data

--- tests/testthat/test_marginal_likelihood.R
+++ tests/testthat/test_marginal_likelihood.R
@@ -18,3 +18,27 @@ test_that("overfit model", {
 
     expect_warning(marginalLikelihood(fit))
 })
+
+test_that("batch overfit galaxy", {
+    set.seed(1)
+
+    # load data
+    library(MASS)
+    data(galaxies)
+
+    # correct 78th observation
+    galaxies[78] <- 26960
+
+    mp <- McmcParams(thin=10, iter=1000, burnin=5000, nStarts=1)
+    hypp <- Hyperparameters(type="batch")
+    model <- BatchModel(data=c(galaxies / 1000, 
+                               galaxies / 1000 + 50),
+                        batch=rep(1:2, each=length(galaxies)),
+                        mcmc.params=mp)
+
+    fit <- posteriorSimulation(model, k=1:4)
+    es <- lapply(fit, function(x) effectiveSize(theta(chains(x))))
+    means <- sapply(es, function(x) mean(x))
+
+    marginalLikelihood(fit)
+})
