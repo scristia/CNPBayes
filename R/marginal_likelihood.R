182946f1040cfef80de0eb7b163aabe38246b847
Jacob Carey <jacobcvt12@gmail.com>
2015-11-16 17:52:58 -0500

    Change reduced gibbs for theta to use model
    Not model.reduced. Have to have chains for reduced gibbs of theta (for
    SB, MB, and SBPV). For now, just grab 1:iter(mp) of ptheta.star. Will
    need to throw an error if the chains are not long enough. There may be
    a better way to do this, so that unnecessary calculations are not
    performed (i.e. chains of model are longer than reduced gibbs
    iterations)

--- R/marginal_likelihood.R
+++ R/marginal_likelihood.R
@@ -2,7 +2,7 @@
     model.reduced <- model
     mcmcParams(model.reduced, force=TRUE) <- mp
   
-    ptheta.star <- marginal_theta(model.reduced)
+    ptheta.star <- marginal_theta(model)[1:iter(mp)]
     small.theta.red <- mean(ptheta.star < reject.threshold)
     if(paramUpdates(model)[["theta"]]==0) small.theta.red <- 0
     
@@ -55,7 +55,7 @@
     ##
     ## Block updates for stage 1 parameters
     ##
-    ptheta.star <- full_theta_pooled(model.reduced)
+    ptheta.star <- full_theta_pooled(model)[1:iter(mp)]
     ## Doesn't make sense if theta is fixed
     small.theta.red <- mean(ptheta.star < reject.threshold)
     if(paramUpdates(model)[["theta"]]==0) small.theta.red <- 0
@@ -105,7 +105,7 @@
     model.reduced <- model
     mcmcParams(model.reduced, force=TRUE) <- mp
   
-    ptheta.star <- marginal_theta_batch(model.reduced)
+    ptheta.star <- marginal_theta_batch(model)[1:iter(mp)]
     small.theta.red <- mean(ptheta.star < reject.threshold)
     if(paramUpdates(model)[["theta"]]==0) small.theta.red <- 0
     
