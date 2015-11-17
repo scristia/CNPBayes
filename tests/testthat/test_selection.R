33eb7914909031d5372fe49ec68c01989d5a2e40
Jacob Carey <jacobcvt12@gmail.com>
2015-11-17 13:24:13 -0500

    Change nStarts to 5

--- tests/testthat/test_selection.R
+++ tests/testthat/test_selection.R
@@ -12,7 +12,7 @@ test_that("galaxy model", {
     galaxies[78] <- 26960
 
     # fit model
-    mp <- McmcParams(thin=10, iter=1000, burnin=10000, nStarts=20)
+    mp <- McmcParams(thin=10, iter=1000, burnin=10000, nStarts=5)
     hypp <- Hyperparameters(type="marginal", k=3, m2.0=100)
     model <- MarginalModel(data=galaxies/1000, k=3,
                            hypp=hypp,
@@ -27,7 +27,7 @@ test_that("galaxy model", {
                                                          reject.threshold=1e-50,
                                                          prop.threshold=0.5)))
     marginal_k3 <- unname(m.y[1])
-    expect_equal(marginal_k3, published.mlik, tolerance=5, scale=1)
+    expect_equal(marginal_k3, published.mlik, tolerance=1, scale=1)
 
     # check that model is not overfit
     expect_true(which.max(m.y) == 1L)
