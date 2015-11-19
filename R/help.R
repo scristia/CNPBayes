8cd668b55379d22fee777c063c67bc0378427326
Jacob Carey <jacobcvt12@gmail.com>
2015-11-19 11:06:40 -0500

    Remove oligoClasses import

--- R/help.R
+++ R/help.R
@@ -17,6 +17,5 @@
 #' @importFrom graphics lines par
 #' @importFrom stats dnorm kmeans ks.test plot.ts qgamma rbeta rgamma rgeom rnorm runif setNames
 #' @importFrom coda effectiveSize
-#' @importMethodsFrom oligoClasses lrr baf
 #' @useDynLib CNPBayes
 NULL
