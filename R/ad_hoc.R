83e8094c40dcbce8e6189e032c20c28ce010f097
Jacob Carey <jacobcvt12@gmail.com>
2015-12-14 21:59:28 -0500

    Get means and variances of model in ad hoc

--- R/ad_hoc.R
+++ R/ad_hoc.R
@@ -1,8 +1,16 @@
 label_components <- function(model) {
+    # default to all four copy number gain/deletion
     labels <- c("Homozygous Deletion",
                 "Hemizygous Deletion",
                 "Normal",
                 "Copy Number Gain")
+
+    # get overall means of components
+    means <- mu(model)
+
+    # get overall variances of components
+    # N.B. that this accessor may need to change
+    vars <- colMeans(sigma2(model))
 
     return(labels)
 }
