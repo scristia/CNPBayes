812589bc88b2e2fb465d853d2f87747b77b2b0f4
Jacob Carey <jacobcvt12@gmail.com>
2015-12-14 21:39:40 -0500

    Default to all four component states

---
+++ R/ad_hoc.R
@@ -0,0 +1,8 @@
+label_components <- function(model) {
+    labels <- c("Homozygous Deletion",
+                "Hemizygous Deletion",
+                "Normal",
+                "Copy Number Gain")
+
+    return(labels)
+}
