9522689d029177c607b9c29549bb81aae4660cdd
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-01-07 02:27:42 +0000

    fix imports
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@112263 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/help.R
+++ R/help.R
@@ -2,18 +2,14 @@
 #'
 #' @docType package
 #' @name CNPBayes
-#' @import methods
+#' @import methods BiocGenerics S4Vectors IRanges GenomicRanges 
 #' @importFrom gtools rdirichlet ddirichlet
 #' @importFrom GenomeInfoDb seqinfo seqlevels<- seqlevels seqinfo<- seqnames
-#' @import IRanges
 #' @importFrom Rcpp evalCpp
-#' @importFrom S4Vectors SimpleList DataFrame
-#' @import GenomicRanges 
 #' @importFrom combinat permn
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom foreach %do% foreach
 #' @importFrom matrixStats colSds colMedians rowCumsums rowProds colMaxs
-#' @importFrom BiocGenerics unlist
 #' @importMethodsFrom oligoClasses lrr baf
 #' @useDynLib CNPBayes
 NULL
