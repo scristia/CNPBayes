71afa2a539b369fafc75d5df9e2a79218ff04792
j.carey <j.carey@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-09 00:02:02 +0000

    Add function for using mean summary instead of med
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@114399 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rounded-number-sim.R
+++ R/rounded-number-sim.R
@@ -25,6 +25,33 @@ check_rounding_sim <- function() {
     m.lik <- marginalLikelihood(m.list)
 }
 
+check_rounding_sim_mean <- function() {
+    library(dplyr)
+    set.seed(1)
+    N <- 7524
+
+    X <- rnorm(N * 81, 0, 1)
+    X_round <- as.integer(1000 * X) / 1000
+    lrr <- data.frame(x=X_round, label=rep(1:N, each=81)) %>%
+        group_by(label) %>%
+        summarise(y=mean(x)) %>%
+        .$y
+
+    mp <- McmcParams(iter=1000, burnin=1000, thin=2, nStarts=1)
+
+    # model for data as seen
+    model <- MarginalModel(data=lrr, mcmc.params=mp) 
+    m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
+
+    # model for data w/ random noise
+    model <- MarginalModel(data=lrr + runif(N, 0, 1) / 1000, 
+                           mcmc.params=mp) 
+
+    m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
+}
+
 check_less_rounding_sim <- function() {
     library(dplyr)
     set.seed(1)
