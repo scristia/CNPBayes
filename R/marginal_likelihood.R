a69d2f5d0a50c1f1f4917872c03d523fa7bd437e
j.carey <j.carey@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-09 00:11:43 +0000

    Change blockupdates for marginal for overfit diag
    Break up function into two functions as well
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@114447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/marginal_likelihood.R
+++ R/marginal_likelihood.R
@@ -1,55 +1,53 @@
-blockUpdates <- function(model, mp){
-  ##
-  ## Block updates for stage 1 parameters
-  ##
-  pstar <- setNames(rep(NA, 7),
-                    c("theta",
-                      "sigma",
-                      "pi",
-                      "mu",
-                      "tau",
-                      "nu0",
-                      "s20"))
+.blockUpdates <- function(model, mp, reject.threshold, prop.threshold) {
+    model.reduced <- model
+    mcmcParams(model.reduced, force=TRUE) <- mp
+  
+    ptheta.star <- marginal_theta(model.reduced)
+    small.theta.red <- mean(ptheta.star < reject.threshold)
 
-  ptheta.star <- marginal_theta(model)
-  pstar["theta"] <- log(mean(ptheta.star))
+    if (small.theta.red >= reject.threshold) {
+        warning("The model for k=", k(model), " may be overfit.",
+                " This can lead to an incorrect marginal likelihood")
+        return(matrix(NA))
+    }
+  
+    model.psigma2 <- reduced_sigma(model.reduced)
+    identical(modes(model.psigma2), modes(model))
+    psigma.star <- p_sigma_reduced(model.psigma2)
+  
+    model.pistar <- reduced_pi(model.reduced)
+    identical(modes(model.pistar), modes(model))
+    p.pi.star <- p_pmix_reduced(model.pistar)
+  
+    ##
+    ## Block updates for stage 2 parameters
+    ##
+    model.mustar <- reduced_mu(model.reduced)
+    stopifnot(identical(modes(model.mustar), modes(model)))
+    p.mustar <- p_mu_reduced(model.mustar)
+  
+    model.taustar <- reduced_tau(model.reduced)
+    identical(modes(model.taustar), modes(model))
+    p.taustar <- p_tau_reduced(model.mustar)
+  
+    model.nu0star <- reduced_nu0(model.reduced)
+    identical(modes(model.nu0star), modes(model))
+    p.nu0star <- p_nu0_reduced(model.nu0star)
+  
+    model.s20star <- reduced_s20(model.reduced)
+    p.s20star <- p_s20_reduced(model.s20star)
+  
+    reduced_gibbs <- cbind(ptheta.star, psigma.star, p.mustar, p.pi.star,
+                           p.taustar, p.nu0star, p.s20star)
+  
+    colnames(reduced_gibbs) <- c("theta", "sigma", "pi", "mu",
+                                 "tau", "nu0", "s20")
+  
+    reduced_gibbs
+}
 
-  model.reduced <- model
-  mcmcParams(model.reduced, force=TRUE) <- mp
-
-  model.psigma2 <- reduced_sigma(model.reduced)
-  identical(modes(model.psigma2), modes(model))
-  psigma.star <- p_sigma_reduced(model.psigma2)
-  pstar["sigma"] <- log(mean(psigma.star))
-
-
-  model.pistar <- reduced_pi(model.reduced)
-  identical(modes(model.pistar), modes(model))
-  p.pi.star <- p_pmix_reduced(model.pistar)
-  pstar["pi"] <- log(mean(p.pi.star))
-  ##
-  ## Block updates for stage 2 parameters
-  ##
-  model.mustar <- reduced_mu(model.reduced)
-  stopifnot(identical(modes(model.mustar), modes(model)))
-  p.mustar <- p_mu_reduced(model.mustar)
-  pstar["mu"] <- log(mean(p.mustar))
-
-  model.taustar <- reduced_tau(model.reduced)
-  identical(modes(model.taustar), modes(model))
-  p.taustar <- p_tau_reduced(model.mustar)
-  pstar["tau"] <- log(p.taustar)
-
-  model.nu0star <- reduced_nu0(model.reduced)
-  identical(modes(model.nu0star), modes(model))
-  p.nu0star <- p_nu0_reduced(model.nu0star)
-  pstar["nu0"] <- log(mean(p.nu0star))
-
-  model.s20star <- reduced_s20(model.reduced)
-  p.s20star <- p_s20_reduced(model.s20star)
-  pstar["s20"] <- log(p.s20star)
-
-  pstar
+blockUpdates <- function(reduced_gibbs, root) {
+    pstar <- apply(reduced_gibbs, 2, function(x) log(mean(x^(root)))),
 }
 
 
