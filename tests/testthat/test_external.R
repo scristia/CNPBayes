f2b069dbc160eb6ad841ed151f7d02b79bab7066
j.carey <j.carey@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-09 00:00:36 +0000

    Add test that runs BiocCheck
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@114391 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test_external.R
@@ -0,0 +1,20 @@
+test_that("BiocCheck", {
+    # run BiocCheck on package
+    library(BiocCheck)
+    check <- BiocCheck(".")
+
+    # pull out the errors/warnings/messages into separate variables
+    requirements <- check$requirements
+    recommendations <- check$recommendations
+    considerations <- check$considerations
+
+    # warnings that are known and ignored
+    ignore <- c("Remove vignette sources from inst/doc; they belong in vignettes/.",
+                "y of x.y.z version should be even in release",
+                "Register native routines! see http://cran.r-project.org/doc/manuals/R-exts.html#Registering-native-routines")
+    recommendations <- recommendations[!recommendations %in% ignore]
+
+    # check that there are no errors
+    expect_true(length(requirements) == 0)
+    expect_true(length(recommendations) == 0)
+})
