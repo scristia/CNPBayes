3842faa3fa7b33b998dbcf48991a25114bc10e4f
Jacob Carey <jcarey15@jhu.edu>
2016-03-09 00:01:20 +0000

    Add function with less rounding and try "fix"
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CNPBayes@114395 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rounded-number-sim.R
+++ R/rounded-number-sim.R
@@ -1,15 +1,53 @@
 check_rounding_sim <- function() {
-    set.seed(1)
     library(dplyr)
-    X <- rnorm(7524 * 81, 0, 1)
+    set.seed(1)
+    N <- 7524
+
+    X <- rnorm(N * 81, 0, 1)
     X_round <- as.integer(1000 * X) / 1000
-    lrr <- data.frame(x=X_round, label=rep(1:7524, each=81)) %>%
+    lrr <- data.frame(x=X_round, label=rep(1:N, each=81)) %>%
         group_by(label) %>%
         summarise(y=median(x)) %>%
         .$y
 
     mp <- McmcParams(iter=1000, burnin=1000, thin=5, nStarts=10)
+
+    # model for data as seen
     model <- MarginalModel(data=lrr, mcmc.params=mp) 
+    m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
+
+    # model for data w/ random noise
+    model <- MarginalModel(data=lrr + runif(N, 0, 1) / 1000, 
+                           mcmc.params=mp) 
 
     m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
+}
+
+check_less_rounding_sim <- function() {
+    library(dplyr)
+    set.seed(1)
+    N <- 7524
+
+    X <- rnorm(N * 81, 0, 1)
+    X_round <- as.integer(round(1000 * X)) / 1000
+    lrr <- data.frame(x=X_round, label=rep(1:N, each=81)) %>%
+        group_by(label) %>%
+        summarise(y=median(x)) %>%
+        .$y
+
+    mp <- McmcParams(iter=1000, burnin=1000, thin=5, nStarts=10)
+
+    # model for data as seen
+    model <- MarginalModel(data=lrr, mcmc.params=mp) 
+    m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
+
+    # model for data w/ random noise
+    model <- MarginalModel(data=lrr + runif(N, 0, 1) / 100, 
+                           mcmc.params=mp) 
+
+    m.list <- posteriorSimulation(model, k=1:4)
+    m.lik <- marginalLikelihood(m.list)
 }
