ddad86c96c06b8582f52d6c865db14896b12eb24
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2016-12-31 09:17:22 -0500

    Update MultiBatch model unit tests

--- tests/testthat/test_batch.R
+++ tests/testthat/test_batch.R
@@ -11,18 +11,27 @@ test_that("test_batch_moderate", {
     N <- 1000
     truth <- simulateBatchData(N = N, batch = rep(letters[1:3],
                                                   length.out = N),
-                               p = c(1/10, 1/5, 1 - 0.1 - 0.2), theta = means,
+                               p = c(1/10, 1/5, 1 - 0.1 - 0.2),
+                               theta = means,
                                sds = sds)
-    mcmcp <- McmcParams(iter = 1000, burnin = 500, thin = 1,
-                        nStarts = 10)
-
+    ##mcmcp <- McmcParams(iter = 1000, burnin = 500, thin = 1,
+    ##                    nStarts = 10)
+    mcmcp <- McmcParams(iter = 1000, burnin = 0, thin=10,
+                        nStarts = 1000)
     ## this parameter setting for m2.0 allows a lot of varation of the thetas
     ## between batch
-    hypp <- CNPBayes:::HyperparametersBatch(m2.0 = 1/60, eta.0 = 1800,
-                                            k = 3, a = 1/6, b = 180)
+    ##    hypp <- CNPBayes:::HyperparametersBatch(m2.0 = 1/60, eta.0 = 1800,
+    ##                                            k = 3, a = 1/6, b = 180)
     model <- BatchModel(data = y(truth), batch = batch(truth),
-                        k = 3, mcmc.params = mcmcp, hypp = hypp)
+                        k = 3, mcmc.params = mcmcp) ##, hypp = hypp)
+    ## first iteration has the largest log lik.
+    ## -- why is the model not finding nearby regions of higher posterior probability
     model <- posteriorSimulation(model)
+    model <- useModes(model)
+    ##nStarts(model) <- 0
+    ## run additional MCMC iterations
+    ##model <- posteriorSimulation(model)
+    ##plist <- CNPBayes:::ggMultiBatchChains(model)
     i <- order(theta(model)[1, ])
     expect_equal(theta(truth), theta(model)[, i], tolerance=0.1)
     if (FALSE) {
@@ -32,7 +41,7 @@ test_that("test_batch_moderate", {
         plot.ts(pic(modelk), plot.type = "single")
         abline(h = p(truth))
         par(mfrow = c(1, 3))
-        tracePlot(modelk, "theta", col = 1:3)
+        trace(modelk, "theta", col = 1:3)
         abline(h = theta(truth))
         plot.ts(sigmac(modelk), plot.type = "single")
         abline(h = sigma(truth))
@@ -90,16 +99,22 @@ test_that("test_batchEasy", {
         hist(theta, breaks = 100, xlim = c(-2, 1.5))
     }
     set.seed(1)
-    mcmcp <- McmcParams(iter = 300, burnin = 300, nStarts = 5)
+    ##mcmcp <- McmcParams(iter = 300, burnin = 300, nStarts = 5)
+    mcmcp <- McmcParams(iter = 0, burnin = 0, nStarts = 1000, thin=10)
     model <- BatchModel(y(truth), batch = batch(truth), k = 3,
-        mcmc.params = mcmcp)
+                        mcmc.params = mcmcp)
+    ## runs 300 * 5 iterations +  300 iterations after burnin
     model <- posteriorSimulation(model)
+    nStarts(model) <- 0
+    iter(model, force=TRUE) <- 500
+    model2 <- posteriorSimulation(model)
+    model3 <- posteriorSimulation(model2)
     if (FALSE) {
         BatchModelExample <- model
         save(BatchModelExample, file = "data/BatchModelExample.RData")
     }
-    i <- order(theta(model)[1, ])
-    expect_equal(theta(truth), theta(model)[, i], tolerance=0.1)
+    i <- order(theta(model2)[1, ])
+    expect_equal(theta(truth), theta(model3)[, i], tolerance=0.1)
     if (FALSE) {
         op <- par(mfrow = c(1, 2), las = 1)
         plot(truth)
@@ -112,77 +127,70 @@ test_that("test_batchEasy", {
     }
 })
 
+
+hardTruth <- function(p1, s, N=1000){
+  set.seed(1234)
+  k <- 3
+  nbatch <- 3
+  means <- matrix(c(-1.9, -2, -1.85,
+                    -0.45, -0.4, -0.35,
+                    -0.1, 0, -0.05), nbatch, k, byrow=FALSE)
+  sds <- matrix(s, nbatch, k)
+  ##p1 <- prop_comp1
+  p2 <- 20*p1
+  p3 <- 1-p1-p2
+  ##N <- 2000
+  truth <- simulateBatchData(N,
+                             theta=means,
+                             sds=sds,
+                             batch=rep(letters[1:3], length.out=N),
+                             p=c(p1, p2, p3))
+  truth
+}
+
 test_that("test_hard3", {
   library(SummarizedExperiment)
-    # embed function in test for now
-    hardTruth <- function(prop_comp1=0.005, s=0.3) {
-      set.seed(1234)
-      k <- 3
-      nbatch <- 3
-      means <- matrix(c(-1.9, -2, -1.85,
-                        -0.45, -0.4, -0.35,
-                        -0.1, 0, -0.05), nbatch, k, byrow=FALSE)
-      sds <- matrix(s, nbatch, k)
-      p1 <- prop_comp1
-      p2 <- 20*p1
-      p3 <- 1-p1-p2
-      N <- 8e3
-      truth <- simulateBatchData(N,
-                                 theta=means,
-                                 sds=sds,
-                                 batch=rep(letters[1:3], length.out=N),
-                                 p=c(p1, p2, p3))
-    }
-    # end function
-    truth <- hardTruth(0.005, s = 0.1)
-    se <- as(truth, "SummarizedExperiment")
-    if (FALSE)
-        hist(oligoClasses::copyNumber(se), breaks = 1000, col = "gray",
-            border = "gray")
-    mcmcp <- McmcParams(iter = 100, burnin = 0)
-    modelk <- BatchModel(data = y(truth), batch = batch(truth),
-        k = 3, mcmc.params = mcmcp, CNPBayes:::HyperparametersBatch(k = 3,
-            m2.0 = 1/60, eta.0 = 1800))
-    modelk <- CNPBayes:::startAtTrueValues(modelk, truth)
-    mmodel <- posteriorSimulation(modelk)
-    if (FALSE) {
-        op <- par(mfrow = c(1, 2), las = 1)
-        CNPBayes::plot(truth)
-        .plotBatch(mmodel)
-        par(op)
-        i <- order(theta(mmodel)[1, ])
-        theta(mmodel)[, i]
-        theta(truth)
-        sigma(model)[, i]
-        sigma(truth)
-        mc <- chains(mmodel)
-        par(mfrow = c(1, 3))
-        tracePlot(mmodel, "sigma", col = 1:3)
-        tracePlot(mmodel, "theta", col = 1:3)
-        plot.ts(sigma(mc), col = "gray", )
-        plot.ts(theta(mc), col = "gray")
-        plot.ts(mu(mc), plot.type = "single", col = 1:3)
-        cbind(colMeans(sigma(mc)), as.numeric(sigma(truth)))
-    }
-    pmns <- CNPBayes:::thetaMean(mmodel)
-    j <- order(pmns[1, ])
-    ps <- CNPBayes:::sigmaMean(mmodel)[, j]
-    pmix <- CNPBayes:::pMean(mmodel)[j]
-    expect_equal(theta(truth), pmns[, j], tolerance=0.04)
-    expect_equal(sigma(truth), ps, tolerance=0.15)
-    expect_equal(p(truth), pmix, tolerance=0.04)
-    mcmcp <- McmcParams(iter = 200, burnin = 100, nStarts = 20)
-    modelk <- BatchModel(data = y(truth), batch = batch(truth),
-        k = 3, mcmc.params = mcmcp, CNPBayes:::HyperparametersBatch(k = 3,
-            m2.0 = 1/60, eta.0 = 1800, tau2.0 = 1000))
-    mmodel <- posteriorSimulation(modelk)
-    pmns <- CNPBayes:::thetaMean(mmodel)
-    j <- order(pmns[1, ])
-    ps <- CNPBayes:::sigmaMean(mmodel)[, j]
-    pmix <- CNPBayes:::pMean(mmodel)[j]
-    expect_equal(theta(truth), pmns[, j], tolerance=0.04)
-    expect_equal(sigma(truth), ps, tolerance=0.15)
-    expect_equal(p(truth), pmix, tolerance=0.04)
+  ## embed function in test for now
+  # end function
+  truth <- hardTruth(0.005, s = 0.1)
+  se <- as(truth, "SummarizedExperiment")
+  ##
+  ## nStarts=0 forces chain to start at true values
+  ##
+  ## - these unit tests verify that the model stays in a region of high
+  ## - posterior prob.
+  mcmcp <- McmcParams(iter = 100, burnin = 0, nStarts=0)
+  modelk <- BatchModel(data = y(truth), batch = batch(truth),
+      k = 3, mcmc.params = mcmcp, CNPBayes:::HyperparametersBatch(k = 3,
+          m2.0 = 1/60, eta.0 = 1800))
+  modelk <- CNPBayes:::startAtTrueValues(modelk, truth)
+  mmodel <- posteriorSimulation(modelk)
+  pmns <- CNPBayes:::thetaMean(mmodel)
+  ##ps <- CNPBayes:::sigmaMean(mmodel)
+  s <- sigma(mmodel)
+  pmix <- CNPBayes:::pMean(mmodel)
+  expect_equal(theta(truth), pmns, tolerance=0.04)
+  expect_equal(p(truth), pmix, tolerance=0.04)
+})
+
+test_that("test_hard4", {
+  library(SummarizedExperiment)
+  set.seed(1234)
+  truth <- hardTruth(p1=0.005, s = 0.1, N=500)
+  se <- as(truth, "SummarizedExperiment")
+  mcmcp <- McmcParams(iter = 0, burnin = 0, nStarts = 1000)
+  modelk <- BatchModel(data = y(truth), batch = batch(truth),
+                       k = 3, mcmc.params = mcmcp)
+  mmodel <- posteriorSimulation(modelk)
+  mcmcParams(mmodel) <- McmcParams(nStarts=0, iter=1000, thin=5)
+  model2 <- posteriorSimulation(mmodel)
+  model2 <- posteriorSimulation(model2)
+  thetas <- theta(model2)
+  pmix <- p(model2)
+  s <- sigma(model2)
+  expect_equal(theta(truth), thetas, tolerance=0.1)
+  expect_equal(sigma(truth)[, 2:3], s[, 2:3], tolerance=0.15)
+  expect_equal(p(truth), pmix, tolerance=0.04)
 })
 
 test_that("test_kbatch", {
