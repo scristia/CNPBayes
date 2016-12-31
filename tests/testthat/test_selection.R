065f11b1b7f0477314bd90661aeef0b78dd2f7b0
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2016-12-31 09:19:00 -0500

    Update calibration of marginal likelihood with galaxy data
    see test_selection

--- tests/testthat/test_selection.R
+++ tests/testthat/test_selection.R
@@ -1,34 +1,23 @@
-context("Model Selection")
+context("Marginal likelihood calibration")
 
 test_that("galaxy model", {
     # set seed
     set.seed(1)
-
     # load data
     library(MASS)
     data(galaxies)
-
     # correct 78th observation
     galaxies[78] <- 26960
-
     # fit model
     mp <- McmcParams(thin=10, iter=1000, burnin=10000, nStarts=5)
     hypp <- Hyperparameters(type="marginal", k=3, m2.0=100)
     model <- MarginalModel(data=galaxies/1000, k=3,
                            hypp=hypp,
                            mcmc.params=mp)
-    fit <- posteriorSimulation(model, k=3:4)
-
+    fit <- posteriorSimulation(model)
     # calculate marginal likelihood and compare to "truth"
-    published.mlik <- -226.791  
-    expect_warning(m.y <- marginalLikelihood(fit,
-                                             params=list(niter=1000,
-                                                         root=1,
-                                                         reject.threshold=1e-50,
-                                                         prop.threshold=0.5)))
-    marginal_k3 <- unname(m.y[1])
-    expect_equal(marginal_k3, published.mlik, tolerance=1, scale=1)
-
-    # check that model is not overfit
-    expect_true(which.max(m.y) == 1L)
+    published.mlik <- -226.791
+    m.y <- marginalLikelihood(fit, mlParams(root=1))
+    m.y <- unname(m.y)
+    expect_equal(m.y, published.mlik, tolerance=1, scale=1)
 })
