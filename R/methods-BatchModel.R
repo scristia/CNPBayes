902e44d2a5c1014bcacc70f6fb78a6d860dfac77
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2016-12-31 08:37:39 -0500

    Adds meltSingleBatchChains -- prepares chains for ggplot vis.

--- R/methods-BatchModel.R
+++ R/methods-BatchModel.R
@@ -459,12 +459,13 @@ multiBatchDensities <- function(object){
   avglrrs <- observed(object)
   quantiles <- seq(min(avglrrs), max(avglrrs), length.out=500)
   batchPr <- table(batch(object))/length(y(object))
-  dens.list <- batchDensities(quantiles, uniqueBatch(object), 
+  dens.list <- batchDensities(quantiles, uniqueBatch(object),
                               thetas, sigmas, P, batchPr)
   ##component <- lapply(dens.list, rowSums)
   ##overall <- rowSums(do.call(cbind, component))
   ix <- order(thetas[1, ])
-  d <- do.call(rbind, dens.list[ix])
+  dens.list <- dens.list[ix]
+  d <- do.call(rbind, dens.list)
   K <- ncol(thetas)
   NB <- nBatch(object)
   over <- Reduce("+", dens.list)
@@ -500,11 +501,11 @@ meltMultiBatchChains <- function(model){
   K <- k(model)
   B <- nBatch(model)
   btch <- matrix(uniqueBatch(model), B, K, byrow=FALSE)
-  btch <- rep(as.character(btch), each=iter(model.1))
+  btch <- rep(as.character(btch), each=iter(model))
   comp <- matrix(1:K, B, K, byrow=TRUE)
-  comp <- rep(as.numeric(comp), each=iter(model.1))
+  comp <- rep(as.numeric(comp), each=iter(model))
   th.m$batch <- factor(btch)
-  th.m$comp <- factor(comp)
+  th.m$comp <- factor(paste0("k=", comp))
   th.m$iter <- as.integer(th.m$iter)
 
   s <- as.data.frame(sigma(ch))
@@ -529,20 +530,23 @@ meltMultiBatchChains <- function(model){
   mus$iter <- th$iter
   mus$param <- "mu"
   mus.m <- melt(mus)
-  mus.m$comp <- factor(rep(seq_len(k(model.1)), each=iter(model.1)))
+  mu.comp <- rep(seq_len(k(model)), each=iter(model))
+  mus.m$comp <- factor(paste0("k=", mu.comp))
 
   prob <- as.data.frame(p(ch))
   prob$iter <- th$iter
   prob$param <- "p"
   prob.m <- melt(prob)
-  prob.m$comp <- factor(rep(seq_len(k(model.1)), each=iter(model.1)))
+  prob.m$comp <- mus.m$comp
 
   taus <- as.data.frame(tau(ch))
   taus$iter <- th$iter
   taus$param <- "tau"
   taus.m <- melt(taus)
-  taus.m$comp <- factor(rep(seq_len(k(model.1)), each=iter(model.1)))
+  ##taus.m$comp <- factor(rep(seq_len(k(model)), each=iter(model)))
+  taus.m$comp <- mus.m$comp
   prob.m$iter <- taus.m$iter <- mus.m$iter <- as.integer(mus.m$iter)
+
   dat.batch <- rbind(th.m,
                      s.m)
   dat.comp <- rbind(mus.m, taus.m, prob.m)
@@ -554,22 +558,104 @@ meltMultiBatchChains <- function(model){
        single=dat)
 }
 
+
+meltSingleBatchChains <- function(model){
+  ch <- chains(model)
+  th <- as.data.frame(theta(ch))
+  th$param <- "theta"
+  th$iter <- factor(1:nrow(th))
+  th.m <- melt(th)
+  ##
+  ## 
+  ##
+  K <- k(model)
+  comp <- rep(seq_len(K), each=nrow(th))
+  th.m$comp <- factor(paste0("k=", comp))
+  th.m$iter <- as.integer(th.m$iter)
+
+  s <- as.data.frame(sigma(ch))
+  s$iter <- th$iter
+  s$param <- "sigma"
+  s.m <- melt(s)
+  s.m$iter <- th.m$iter
+  s.m$comp <- th.m$comp
+
+  nu0 <- as.data.frame(nu.0(ch))
+  nu0$iter <- th$iter
+  nu0$param <- "nu0"
+  nu0.m <- melt(nu0)
+
+  s20 <- as.data.frame(sigma2.0(ch))
+  s20$iter <- th$iter
+  s20$param <- "s20"
+  s20.m <- melt(s20)
+
+  mus <- as.data.frame(mu(ch))
+  mus$iter <- th$iter
+  mus$param <- "mu"
+  mus.m <- melt(mus)
+  ##mus.m$comp <- factor(rep(seq_len(k(model)), each=iter(model)))
+
+  prob <- as.data.frame(p(ch))
+  prob$iter <- th$iter
+  prob$param <- "p"
+  prob.m <- melt(prob)
+  p.comp <- rep(seq_len(k(model)), each=iter(model))
+  prob.m$comp <- factor(paste0("k=", p.comp))
+
+  taus <- as.data.frame(tau(ch))
+  taus$iter <- th$iter
+  taus$param <- "tau"
+  taus.m <- melt(taus)
+  ##taus.m$comp <- factor(rep(seq_len(k(model)), each=iter(model)))
+  ##prob.m$iter <- taus.m$iter <- mus.m$iter <- as.integer(mus.m$iter)
+  taus.m$iter <- as.integer(mus.m$iter)
+  dat.comp <- rbind(th.m,
+                    s.m,
+                    prob.m)
+  dat.comp$iter <- as.integer(dat.comp$iter)
+  dat <- rbind(nu0.m,
+               s20.m,
+               mu=mus.m,
+               tau=taus.m)
+  dat$iter <- as.integer(dat$iter)
+  list(comp=dat.comp,
+       single=dat)
+}
+
+ggSingleBatchChains <- function(model){
+  melt.ch <- meltSingleBatchChains(model)
+  dat.comp <- melt.ch[["comp"]]     ## component-specific
+  dat.single <- melt.ch[["single"]] ## single-parameter
+  p.comp <- ggplot(dat.comp, aes(iter, value, group=comp)) +
+    geom_point(size=0.3, aes(color=comp)) +
+    geom_line(aes(color=comp)) +
+    facet_wrap(~param, scales="free_y")
+  p.single <- ggplot(dat.single, aes(iter, value, group="param")) +
+    geom_point(size=0.3, color="gray") +
+    geom_line(color="gray") +
+    facet_wrap(~param, scales="free_y")
+   list(comp=p.comp, single=p.single)
+}
+
 ggMultiBatchChains <- function(model){
   melt.ch <- meltMultiBatchChains(model)
   dat.batch <- melt.ch$batch
   dat.comp <- melt.ch$comp
   dat.single <- melt.ch$single
-  p.batch <- ggplot(dat.batch, aes(iter, value, group=batch)) + geom_point(size=0.3, aes(color=batch)) +
+  p.batch <- ggplot(dat.batch, aes(iter, value, group=batch)) +
+    geom_point(size=0.3, aes(color=batch)) +
     geom_line(aes(color=batch)) +
     facet_grid(param ~ comp, scales="free_y")
 
-  p.comp <- ggplot(dat.comp, aes(iter, value, group=comp)) + geom_point(size=0.3, aes(color=comp)) +
+  p.comp <- ggplot(dat.comp, aes(iter, value, group=comp)) +
+    geom_point(size=0.3, aes(color=comp)) +
     geom_line(aes(color=comp)) +
     facet_wrap(~param, scales="free_y")
 
-  p.single <- ggplot(dat.single, aes(iter, value, group="param")) + geom_point(size=0.3, color="gray") +
+  p.single <- ggplot(dat.single, aes(iter, value, group="param")) +
+    geom_point(size=0.3, color="gray") +
     geom_line(color="gray") +
     facet_wrap(~param, scales="free_y")
-
   list(batch=p.batch, comp=p.comp, single=p.single)
 }
