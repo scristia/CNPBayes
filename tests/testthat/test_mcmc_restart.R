ad197a32465c528084560fab43f685fc51ec8dc7
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-01-03 20:32:21 -0500

    Update unit test for mcmc restart

--- tests/testthat/test_mcmc_restart.R
+++ tests/testthat/test_mcmc_restart.R
@@ -7,17 +7,26 @@ test_that("test_mcmc_restart", {
     model <- truth
     mcmcParams(model, force = TRUE) <- McmcParams(iter = 1, burnin = 0, nStarts=0)
     model <- posteriorSimulation(model)
+    ##tmp=replicate(20, theta(posteriorSimulation(model)))
     mc <- chains(model)
     expect_identical(theta(truth), theta(mc)[1, ])
     expect_identical(sigma(truth), sigma(mc)[1, ])
-    mcmcParams(model) <- McmcParams(iter = 1, burnin = 10)
-    model <- posteriorSimulation(model)
+    model2 <- model
+    mcmcParams(model2) <- McmcParams(iter = 10, burnin = 0, nStarts=0)
+    model2 <- posteriorSimulation(model2)
+    theta.first <- thetac(model2)[1, ]
+    theta.last <- theta(model)
+    expect_identical(theta.first, theta.last)
+
     model <- truth
     mcmcParams(model, force = TRUE) <- McmcParams(iter = 1, burnin = 1)
     model <- posteriorSimulation(model)
     mc <- chains(model)
+    ## not the same as truth because of burnin
     expect_false(identical(theta(mc)[1, ], theta(truth)))
     expect_false(identical(sigma(mc)[1, ], sigma(truth)))
+    ## verify the single iteration after burnin is stored as the first value in
+    ## the chain
     expect_equal(as.numeric(theta(model)), theta(mc)[1, ])
     model <- truth
     mcmcParams(model, force = TRUE) <- McmcParams(iter = 0, burnin = 1)
@@ -52,10 +61,22 @@ test_that("test_mcmc_restart", {
     ##
     ## checks consistency of chain with slot for theta
     ##
-    modelk1 <- BatchModel(data = y(truth), k = 3, mcmc.params = mcmcp,
-        batch = batch(truth))
+    modelk1 <- BatchModel(data = y(truth),
+                          k = 3,
+                          mcmc.params = mcmcp,
+                          batch = batch(truth))
+    modelk1 <- sortComponentLabels(modelk1)
     th1 <- as.numeric(theta(modelk1))
+    ##
+    ## 1 iteration and 0 starts
+    ##
     modelk <- posteriorSimulation(modelk1)
     th2 <- CNPBayes:::thetac(modelk)[1, ]
     expect_identical(th2, th1)
+
+    model2 <- modelk
+    mcmcParams(model2) <- McmcParams(iter=10, nStarts=0)
+    model2 <- posteriorSimulation(model2)
+    th3 <- as.numeric(thetac(model2)[1, ])
+    expect_identical(th3, th2)
 })
