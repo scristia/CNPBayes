d740428675d6b0af15e7f419c20d51520eed16f3
rscharpf <rscharpf@MacBook-Pro.local>
2017-05-18 15:13:17 -0400

    Remove annoying message in collapseBatch

--- R/methods-SummarizedExperiment.R
+++ R/methods-SummarizedExperiment.R
@@ -39,7 +39,6 @@ setMethod("collapseBatch", "numeric", function(object, plate, THR=0.1){
   N <- choose(length(unique(plate)), 2)
   cond2 <- TRUE
   while(N > 1 && cond2){
-    message(".", appendLF=TRUE)
     B <- plate
     plate <- .collapseBatch(object, plate, THR=THR)
     cond2 <- !identical(B, plate)
