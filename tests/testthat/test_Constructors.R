8de2912bd1a583faa015e5206d2b91d435cdd797
rscharpf <rscharpf@MacBook-Pro.local>
2017-07-28 10:04:31 -0400

    Pass integers to mcmcparams in unit tests for constructors

--- tests/testthat/test_Constructors.R
+++ tests/testthat/test_Constructors.R
@@ -22,17 +22,17 @@ test_that("test_constructor", {
     mmod <- MarginalModel(data = y(truth))
     expect_true(validObject(mmod))
     expect_true(iter(mmod) == 1000)
-    iter(mmod) <- 1000
+    iter(mmod) <- 1000L
     expect_error(iter(mmod) <- 1001)
-    iter(mmod, force = TRUE) <- 1001
-    expect_true(iter(mmod) == 1001)
-    expect_true(burnin(mmod) == 100)
-    expect_true(nStarts(mmod) == 1)
+    iter(mmod, force = TRUE) <- 1001L
+    expect_true(iter(mmod) == 1001L)
+    expect_true(burnin(mmod) == 100L)
+    expect_true(nStarts(mmod) == 1L)
     expect_true(nrow(CNPBayes:::thetac(mmod)) == 1001)
-    mp <- McmcParams(iter = 10, thin = 10, burnin = 100)
+    mp <- McmcParams(iter = 10L, thin = 10L, burnin = 100L)
     mcmcParams(mmod) <- mp
     expect_true(nrow(CNPBayes:::thetac(mmod)) == 10)
-    iter(mmod, force = TRUE) <- 1000
+    iter(mmod, force = TRUE) <- 1000L
     expect_true(nrow(CNPBayes:::thetac(mmod)) == 1000)
     bmod <- BatchModel()
     expect_true(validObject(bmod))
@@ -52,19 +52,19 @@ test_that("test_constructor", {
     expect_equal(batch(bmod), batch(truth))
     expect_error(BatchModel(data = y(truth), batch = rep(1:3,
         each = 2), k = 3))
-    expect_true(iter(bmod) == 1000)
-    expect_true(burnin(bmod) == 100)
-    expect_true(nStarts(bmod) == 1)
+    expect_true(iter(bmod) == 1000L)
+    expect_true(burnin(bmod) == 100L)
+    expect_true(nStarts(bmod) == 1L)
     expect_true(nrow(CNPBayes:::thetac(bmod)) == 1000)
-    expect_error(iter(bmod) <- 10)
-    iter(bmod, force = TRUE) <- 10
+    expect_error(iter(bmod) <- 10L)
+    iter(bmod, force = TRUE) <- 10L
     expect_true(nrow(CNPBayes:::thetac(bmod)) == 10)
-    iter(mmod, force = TRUE) <- 5
-    burnin(mmod) <- 0
+    iter(mmod, force = TRUE) <- 5L
+    burnin(mmod) <- 0L
     mmod2 <- posteriorSimulation(mmod)
     expect_false(identical(CNPBayes:::thetac(mmod), CNPBayes:::thetac(mmod2)))
     expect_true(all(is.na(CNPBayes:::thetac(mmod))))
-    burnin(mmod2) <- 2
+    burnin(mmod2) <- 2L
     expect_false(all(is.na(CNPBayes:::thetac(mmod2))))
 
     truth <- simulateData(N = 2500, p = rep(1/3, 3),
