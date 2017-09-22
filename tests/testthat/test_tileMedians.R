79838189ad4eeff86da8207c5b359ef8b73a1f93
rscharpf <rscharpf@MacBook-Pro.local>
2017-09-22 15:41:54 -0400

    Add tileMedians unit test

---
+++ tests/testthat/test_tileMedians.R
@@ -0,0 +1,19 @@
+context("Tile median log R ratios")
+
+test_that("tileMedians", {
+  mb <- MultiBatchModelExample
+  tiled.medians <- tileMedians(y(mb), 200, batch(mb))
+  expected <- tiled.medians %>% group_by(tile) %>%
+    summarize(avgLRR=mean(logratio),
+              batch=unique(batch))
+  tile.summaries <- tileSummaries(tiled.medians)
+  expect_identical(tile.summaries, expected)
+  mp <- McmcParams(iter=50, burnin=100)
+  mb <- MultiBatchModel2(dat=tile.summaries$avgLRR,
+                         batches=tile.summaries$batch,
+                         mp=mp)
+  mb <- posteriorSimulation(mb)
+  ggMixture(mb)
+  mb2 <- upSample(mb, tiled.medians)
+  expect_identical(length(y(mb2)), nrow(tiled.medians))
+}
