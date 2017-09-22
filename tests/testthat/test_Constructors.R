b7d4bca273000ec211ffd98e0350d011e19aeec0
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2017-09-21 21:52:46 -0400

    Update constructors unit test

--- tests/testthat/test_Constructors.R
+++ tests/testthat/test_Constructors.R
@@ -8,7 +8,7 @@ test_that("test_constructor", {
     hypp <- Hyperparameters("batch")
     expect_true(validObject(hypp))
     ##expect_warning(mmod <- SingleBatchModel())
-    mmod <- SingleBatchModel()
+    mmod <- SingleBatchModel2()
     expect_true(validObject(mmod))
     mc <- McmcChains()
     expect_true(validObject(mc))
@@ -18,23 +18,21 @@ test_that("test_constructor", {
     truth <- simulateData(N = 2500, p = rep(1/3, 3), theta = c(-1,
         0, 1), sds = rep(0.1, 3))
     expect_true(validObject(truth))
-    mmod <- SingleBatchModel(data = rnorm(100))
-    mmod <- SingleBatchModel(data = y(truth))
+    mmod <- SingleBatchModel2(dat=rnorm(100))
+    mmod <- SingleBatchModel2(dat=y(truth))
     expect_true(validObject(mmod))
-    expect_true(iter(mmod) == 1000)
     iter(mmod) <- 1000L
     expect_error(iter(mmod) <- 1001)
     iter(mmod, force = TRUE) <- 1001L
+    burnin(mmod) <- 1001L
     expect_true(iter(mmod) == 1001L)
-    expect_true(burnin(mmod) == 100L)
-    expect_true(nStarts(mmod) == 1L)
     expect_true(nrow(thetac(mmod)) == 1001)
     mp <- McmcParams(iter = 10L, thin = 10L, burnin = 100L)
     mcmcParams(mmod) <- mp
     expect_true(nrow(thetac(mmod)) == 10)
     iter(mmod, force = TRUE) <- 1000L
     expect_true(nrow(thetac(mmod)) == 1000)
-    bmod <- MultiBatchModel()
+    bmod <- MultiBatchModel2()
     expect_true(validObject(bmod))
     k <- nbatch <- 3
     means <- matrix(c(-1.2, -1, -0.8, -0.2, 0, 0.2, 0.8, 1, 1.2),
@@ -44,19 +42,15 @@ test_that("test_constructor", {
         length.out = 2500), theta = means, sds = sds, p = c(1/5,
         1/3, 1 - 1/3 - 1/5))
     expect_true(validObject(truth))
-    bmod <- MultiBatchModel(data = y(truth), k = 3)
-    expect_true(is(bmod, "SingleBatchModel"))
-    bmod <- MultiBatchModel(data = y(truth), batch = batch(truth))
+    bmod <- MultiBatchModel2(dat=y(truth), hp=hpList(k=3)[["MB"]],
+                             batches=batch(truth))
+    expect_true(is(bmod, "MultiBatchModel"))
+    bmod <- MultiBatchModel2(dat=y(truth), batches=batch(truth))
     expect_true(is(bmod, "MultiBatchModel"))
     expect_equal(y(bmod), y(truth))
     expect_equal(batch(bmod), batch(truth))
-    expect_error(MultiBatchModel(data = y(truth), batch = rep(1:3,
-        each = 2), k = 3))
-    expect_true(iter(bmod) == 1000L)
-    expect_true(burnin(bmod) == 100L)
-    expect_true(nStarts(bmod) == 1L)
-    expect_true(nrow(thetac(bmod)) == 1000)
-    expect_error(iter(bmod) <- 10L)
+    expect_error(MultiBatchModel2(dat=y(truth),
+                                  batches=rep(1:3, each=2), k=3))
     iter(bmod, force = TRUE) <- 10L
     expect_true(nrow(thetac(bmod)) == 10)
     iter(mmod, force = TRUE) <- 5L
@@ -66,7 +60,6 @@ test_that("test_constructor", {
     expect_true(all(is.na(thetac(mmod))))
     burnin(mmod2) <- 2L
     expect_false(all(is.na(thetac(mmod2))))
-
     truth <- simulateData(N = 2500, p = rep(1/3, 3),
                           theta = c(-1, 0, 1), sds = rep(0.1, 3))
     hp <- Hyperparameters(k=5,
