079a17f2880d2f3477913e2a17aed60cbe2e64dc
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2018-02-12 20:19:28 -0500

    Update data/ objects with degrees of freedom slot

--- data-raw/examples.R
+++ data-raw/examples.R
@@ -5,3 +5,29 @@ save(SingleBatchModelExample, file="../data/SingleBatchModelExample.rda")
 load("../data/BatchModelExample.RData")
 MultiBatchModelExample <- as(BatchModelExample, "MultiBatchModel")
 save(MultiBatchModelExample, file="../data/MultiBatchModelExample.rda")
+
+## updating these examples after migration to heavy-tails
+model <- updateObject(MultiBatchPooledExample)
+hp <- hyperParams(model)
+hp@dfr <- 100
+hyperParams(model) <- hp
+model@u <- rchisq(length(y(model)), hp@dfr)
+MultiBatchPooledExample <- model
+save(MultiBatchPooledExample, file="../data/MultiBatchPooledExample.rda")
+
+model <- updateObject(MultiBatchModelExample)
+hp <- hyperParams(model)
+hp@dfr <- 100
+hyperParams(model) <- hp
+model@u <- rchisq(length(y(model)), hp@dfr)
+MultiBatchModelExample <- model
+save(MultiBatchModelExample, file="../data/MultiBatchModelExample.rda")
+
+
+model <- updateObject(SingleBatchModelExample)
+hp <- hyperParams(model)
+hp@dfr <- 100
+hyperParams(model) <- hp
+model@u <- rchisq(length(y(model)), hp@dfr)
+SingleBatchModelExample <- model
+save(SingleBatchModelExample, file="../data/SingleBatchModelExample.rda")
