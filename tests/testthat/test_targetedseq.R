f41072b095d5a0881dd31af1c21a115e33db1eb6
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2018-02-15 06:32:54 -0500

    Make targeted seq unit test faster

--- tests/testthat/test_targetedseq.R
+++ tests/testthat/test_targetedseq.R
@@ -1,15 +1,19 @@
 context("Targeted seq")
 
+##
+## model selection takes too long
+## - need to put this in CNPBayesUnitTests
+##
 test_that("targeted_seq data", {
   set.seed(123)
-  mp <- McmcParams(iter=500, burnin=1000, nStarts=5)
+  mp <- McmcParams(iter=100, burnin=50, nStarts=4)
   extfile <- file.path(system.file("extdata", package="CNPBayes"),
                        "targeted_seq.txt")
   dat <- read.delim(extfile)[[1]]
   dat <- sample(dat, 500)
   mlist <- gibbs_K(mp=mp, dat=dat, k_range=c(2, 3),
-                   max_burnin=2000,
-                   min_effsize=200)
+                   max_burnin=50,
+                   min_effsize=25)
   ##
   ## Select k=3
   ##
