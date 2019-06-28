2049160cb92b6b88a6aac52656c823fd77578e0e
Robert Scharpf <rscharpf@Roberts-MacBook-Pro-2.local>
2019-06-28 16:50:46 -0400

    Deleted some unit tests in multibatch

--- tests/testthat/test_posterior_predictive.R
+++ tests/testthat/test_posterior_predictive.R
@@ -8,16 +8,6 @@ test_that("MBP", {
   expect_identical(length(sigma(mbp.tmp)), 3L)
   ch <- sigma2(chains(mbp.tmp))
   expect_identical(dim(ch), c(iter(mb), 3L))
-
-  hp <- hpList(k=3)[["MBP"]]
-  mbp <- MultiBatchPooled(dat=y(mb),
-                          hp=hp,
-                          mp=mp,
-                          batches=batch(mb))
-  ch <- sigma2(chains(mbp))
-  expect_identical(dim(ch), c(iter(mbp), 3L))
-  mbp <- posteriorSimulation(mbp)
-  mbp2 <- as(mbp, "MultiBatchP")
-  fl <- flags(mbp2)
-  expect_true(fl$label_switch)
 })
+
+
