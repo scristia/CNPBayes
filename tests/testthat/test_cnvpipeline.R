66816e74498778510cc003caf7785def33ec84cb
rscharpf <rscharpf@jhu.edu>
2019-07-21 21:36:10 -0400

    add unit test for cnv pipeline

---
+++ tests/testthat/test_cnvpipeline.R
@@ -0,0 +1,10 @@
+test_that("cnv pipeline", {
+  library(SummarizedExperiment)
+  data(snp_se, package="panc.data")
+  path <- system.file("extdata", package="CNPBayes")
+  mb <- readRDS(file.path(path, "mb_subsamp.rds"))
+  g <- rowRanges(cnp_se)[1]
+  model <- cnv_models(mb, g, snp_se)
+  expect_identical(modelName(model), "MBP3")
+  expect_identical(mapping(model), c("0", "2", "2"))
+})
