d5ee81d035ea8382661af46c76a356d0f465da4f
rscharpf <rscharpf@jhu.edu>
2019-07-26 07:06:52 -0400

    clean data.R

--- R/data.R
+++ R/data.R
@@ -1,23 +1,3 @@
-#' This data is a simulated example of Marginal data
-#'
-#' @docType data
-#' @name MarginalModelExample
-#' @usage MarginalModelExample
-#' @author Jacob Carey
-#' @return An example of a `MarginalModel`
-#' MarginalModelExample
-NULL
-
-#' This data is a simulated example of Batch data
-#'
-#' @docType data
-#' @name BatchModelExample
-#' @usage BatchModelExample
-#' @author Jacob Carey
-#' @return An example of a `BatchModel`
-#' BatchModelExample
-NULL
-
 #' This data is an instance of SingleBatchModel
 #'
 #' @docType data
